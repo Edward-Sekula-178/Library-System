c4ae2ce95330995142b1d49ee3ffa58e231808d8
King-Of-Radar <90248097+King-Of-Radar@users.noreply.github.com>
2021-11-25 10:41:21 +0000

    Implementation of basic php

---
+++ Portal.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<link><styles class="css"></styles></link>
+
+<h>WELCOME TO YOUR LIBRARY PORTAL</h>
+<ul>
+    <li style="link_item"><a href="library.html">Interstellar library service</a></li>
+    <li style="link_item"><a href="Readinglist.htm">Your List</a></li>
+    <li style="link_item"><a href="yourbooks.html">Manage current loans</a></li>
+</ul>
+
+</html>
+
---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<form action="logiprocsess.php" method="POST">
+    User name:<input type="text" name="Username"><br>
+    Password:<input type="text" name="Password"><br>
+    <input type="submit" value="login">
+</form>
+
+</html>
