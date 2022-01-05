106fb8399eeeabed1e6ea8e04b37d6a3f5fe115c
KingOfRadar <86322235+KingOfRadar@users.noreply.github.com>
2022-01-05 20:02:17 +0000

    CURRENT UPDATE

--- Library.php
+++ Library.php
@@ -1,3 +1,51 @@
+<?php
+session_start();
+include_once('functions.php');
+countrows();
+?>
+
+
 <!DOCTYPE html>
 <html>
+<head>
+  <link rel="stylesheet" href="styles.css">
+</head>
+    <div class="sidenav">
+        <form action='searchbridge.php' method='POST'>
+            <input type='radio' name='category' value='null' checked>No category<br>
+            <input type='radio' name='category' value='SCIFI' >Sci-Fi<br>
+            <input type='submit' value='apply'>
+        </form>
+</div>
+<main class = 'main'>
+    <header>
+        <form action='seachbridge.php' method='POST'>
+            Search phrase:<input type=Text name='searchphrase'>
+            <input type='radio' name='field' value='ISBN' checked>Title<br>
+            <input type='radio' name='field' value='author' checked>Author<br>
+            <input type='radio' name='field' value='title' checked>ISBN<br>
+            <input type='submit' value='Apply'>
+        </form>
+    </header>
+    <?php
+        $title=null;
+        $pathvar=null;
+        $element = "<div class='container'>
+                    <img src=$pathvar class='coverobject' >
+                    <a href=bookpage.php>$title</a>
+                    </div>
+                    ";
+    $count = $_SESSION['limit'];
+    foreach( range(1,$count) as $item){
+        include_once('functions.php');
+        $imgtitle=findtitle($item);
+        $pathvar=findimg($imgtitle);
+        echo $pathvar;
+        echo $element;
+    }
+    #echo "<img src='".$row['photo']."' />";
+    ?>
+</main class = 'main'>
+
 </html>
+
